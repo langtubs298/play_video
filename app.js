50989255a16c13995acd4d34e9a126b440ee6137
vienll <vienll@nal.vn>
2021-11-21 13:02:09 +0700

    update source

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var serveStatic = require('serve-static')
 
 var app = express()
 
-app.use(serveStatic('test', { 'index': ['default.html', 'default.htm'] }))
+app.use(serveStatic('save_video', { 'index': ['default.html', 'default.htm'] }))
 app.listen(5000)
