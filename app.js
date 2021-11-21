ab5e06408d761ba88734e42261e5284e12b3146f
vienll <vienll@nal.vn>
2021-11-21 16:54:30 +0700

    move to https

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+const https = require('https');
+const fs = require('fs');
 var express = require('express')
 var serveStatic = require('serve-static')
 
 var app = express()
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
 
 app.use(serveStatic('save_video', { 'index': ['default.html', 'default.htm'] }))
-app.listen(5000)
+https.createServer(options, app)
+    .listen(7000, function (req, res) {
+        console.log("Server started at port 7000");
+    });
